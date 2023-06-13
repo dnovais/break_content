9e21b11bad078688e3f5c3134b97537dc14abbd1
Diego Novais <contato@diegonovais.com.br>
2023-06-13 13:50:07 -0300

    when the content is bigger than limit

--- lib/break_content.rb
+++ lib/break_content.rb
@@ -1,3 +1,34 @@
 class BreakContent
-  def initialize; end
+  def initialize(content, limit)
+    @content = content
+    @limit = limit
+  end
+
+  def call
+    break_content
+  end
+
+  private
+
+  attr_reader :content, :limit
+
+  def break_content
+    words = content.split
+
+    lines = []
+    line = ""
+  
+    words.each do |word|
+      if line.size + word.size + 1 <= limit  # Add 1 for the space after each word
+        line += "#{word} "
+      else
+        lines << line.strip
+        line = "#{word} "
+      end
+    end
+  
+    lines << line.strip unless line.empty?
+  
+    lines.join("\n")
+  end
 end
--- spec/lib/break_content_spec.rb
+++ spec/lib/break_content_spec.rb
@@ -3,5 +3,17 @@ require "break_content"
 
 RSpec.describe BreakContent do
   describe "#call" do
+    context "when the content is bigger than limit" do
+      it "should return the content breaked" do
+        content = "In 1991, while studying computer science at University of Helsinki, Linus Torvalds began a project that later became the Linux kernel. He wrote the program specifically for the hardware he was using and independent of an operating system because he wanted to use the functions of his new PC with an 80386 processor. Development was done on MINIX using the GNU C Compiler."
+        limit = 40
+
+        expected_result = "In 1991, while studying computer science\nat University of Helsinki, Linus\nTorvalds began a project that later\nbecame the Linux kernel. He wrote the\nprogram specifically for the hardware he\nwas using and independent of an\noperating system because he wanted to\nuse the functions of his new PC with an\n80386 processor. Development was done on\nMINIX using the GNU C Compiler."
+
+        result = described_class.new(content, limit).call
+
+        expect(result).to eq(expected_result)
+      end
+    end
   end
 end
